7527aa294c0e7d9f009ea2dd924801ec21f93662
Tyler Busby <TylerBusby333@gmail.com>
2019-05-03 10:23:27 -0500

    Add more complex error handling and retrying

--- data_collector.py
+++ data_collector.py
@@ -6,6 +6,12 @@ import numpy as np
 import requests
 import os
 
+from requests.adapters import HTTPAdapter
+from urllib3 import Retry
+
+MAX_RETRIES = 5
+MAX_FAILURES = 6  # number of intervals that are allowed to fail before quitting
+
 
 def above_five_minutes(seconds_string):
     value = int(seconds_string)
@@ -39,19 +45,46 @@ if __name__ == "__main__":
 
     wait_for_modulo_time(args.seconds_between_requests)
 
+    session = requests.Session()
+
+    retries = Retry(total=MAX_RETRIES,
+                    backoff_factor=5,
+                    status_forcelist=[500, 502, 503, 504])
+
+    session.mount('https://', HTTPAdapter(max_retries=retries))
+
+    failures = 0
     while True:
-        request_time = time.time()
-
-        response = requests.get('https://api.co2signal.com/v1/latest?countryCode={country_code}'.format(
-            country_code=args.country_code), headers={'auth-token': os.environ['CO2SIGNAL_API_KEY']})
-        if response.ok:
-            json_response = response.json()
-            data = json_response['data']
-            data_list = [request_time, data['carbonIntensity'], data['fossilFuelPercentage']]
-            print("{}: {}".format(datetime.fromtimestamp(request_time), data_list))
-            measurements = np.append(measurements, data_list)
-            np.save(data_filename, measurements)
-        else:
-            raise ValueError(response.content)
-
+        success = False
+        for retry_count in range(MAX_RETRIES):
+            request_time = time.time()
+            try:
+                response = session.get('https://api.co2signal.com/v1/latest?countryCode={country_code}'.format(
+                    country_code=args.country_code), headers={'auth-token': os.environ['CO2SIGNAL_API_KEY']})
+                if response.ok:
+                    json_response = response.json()
+                    data = json_response['data']
+                    data_list = [request_time, data['carbonIntensity'], data['fossilFuelPercentage']]
+                    print("{}: {}".format(datetime.fromtimestamp(request_time), data_list))
+                    measurements = np.append(measurements, data_list)
+                    np.save(data_filename, measurements)
+                    failures = 0
+                    success = True
+                    break
+                else:
+                    backoff_time = pow(2, retry_count)
+                    print('Got this response from co2signal:"{error}", exponentially backing off, {time} seconds.'
+                          .format(error=getattr(response, "content", None), time=backoff_time))
+                    time.sleep(backoff_time)
+                    success = False
+            except ConnectionError as e:
+                backoff_time = pow(2, retry_count)
+                print('Got an connection error when trying to query co2signal, exponentially backing off, '
+                      '{time} seconds.'.format(error=repr(e), time=backoff_time))
+                time.sleep(backoff_time)
+        if not success:
+            failures += 1
+            if failures > MAX_FAILURES:
+                raise SystemError('Number of data collection windows missed exceeded maximum failures allowed: {}'
+                                  .format(MAX_FAILURES))
         wait_for_modulo_time(args.seconds_between_requests)
