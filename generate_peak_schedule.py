887e715e3d7c4cfc76f265db1ada3bfa4031cba4
Tyler Busby <TylerBusby333@gmail.com>
2019-05-27 20:00:41 -0500

    finish primitive peak schedule generation

--- generate_peak_schedule.py
+++ generate_peak_schedule.py
@@ -7,10 +7,56 @@ import numpy as np
 
 MINUTE_RESOLUTION = 5  # minutes
 TERTILE_Z_SCORE = 0.43073
+PEAKEDNESS_NAMES_DICT = {
+    1: "Off-Peak",
+    2: "Mid-Peak",
+    3: "Peak"
+}
+
+
+class TimeRange(object):
+    """An object encapsulating a start time and an end time"""
+
+    def __init__(self, start, end, name=None):
+        self.start = start
+        self.end = end
+        self.name = name
+
+    def __str__(self):
+        return "{}-{} {}".format(self.start, self.end, self.name)
+
+    def span(self):
+        return self.end - self.start
+
+
+def boolean_list_to_number(boolean_list):
+    counter = 1
+    for boolean in boolean_list:
+        if boolean:
+            return counter
+        counter += 1
+    raise AssertionError("Internal error, peakedness arrays malformed")
 
 
 def time_of_day_seconds(time):
     return time.hour * 3600 + time.minute * 60 + time.second
+
+
+def create_ranges(aggregated_list):
+    ranges = []
+    start_time = None
+    last_peakedness = None
+    for i, time in enumerate(interval_times):
+        if start_time is None:
+            start_time = time
+            last_peakedness = aggregated_list[i]
+            continue
+        if last_peakedness != aggregated_list[i]:
+            end_time = time
+            ranges.append(TimeRange(start_time, end_time, PEAKEDNESS_NAMES_DICT[last_peakedness]))
+            start_time = time
+            last_peakedness = aggregated_list[i]
+    return ranges
 
 
 def is_weekend(date):
@@ -65,6 +111,23 @@ if __name__ == "__main__":
     weekend_mid_peak_mask = np.isin(interval_times, weekend_mid_peak_times)
     weekend_peak_mask = np.isin(interval_times, weekend_peak_times)
 
-    for i, time in enumerate(interval_times):
-        # TODO aggregate time intervals from masks
-        pass
+    weekday_stacked = np.stack([weekday_off_peak_mask, weekday_mid_peak_mask, weekday_peak_mask])
+    weekday_aggregated = np.apply_along_axis(boolean_list_to_number, 0, weekday_stacked)
+
+    weekend_stacked = np.stack([weekend_off_peak_mask, weekend_mid_peak_mask, weekend_peak_mask])
+    weekend_aggregated = np.apply_along_axis(boolean_list_to_number, 0, weekend_stacked)
+
+    weekday_ranges = create_ranges(weekday_aggregated)
+    weekend_ranges = create_ranges(weekend_aggregated)
+
+    # weekday_ranges = delete_small_ranges(weekday_aggregated)
+    # weekend_ranges = delete_small_ranges(weekend_aggregated)
+
+    print("Weekday Ranges:")
+    for weekday_range in weekday_ranges:
+        print(weekday_range)
+    print()
+    print("Weekend Ranges:")
+    for weekend_range in weekend_ranges:
+        print(weekend_range)
+
